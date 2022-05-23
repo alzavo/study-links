a890a153abfff39f877208cf807f50bdb595a49b
aleks <aleksei.zavoronkov@gmail.com>
2022-05-23 18:17:48 +0300

    design idea implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = createStore({
+    state: {
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
 })
+
+export default store;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,14 @@
+
 <template>
-    <section class="labels">
-        <div class="container">
-            <form
-                @submit.prevent="onSubmit($event)"
-                @change.prevent="getSelectedItems($event)"
-            >
-                <div class="select-container">
-                    <select
-                        v-for="columnWithValues in columnsWithValues"
-                        :key="columnWithValues"
-                    >
-                        <option value="">
-                            {{ columnWithValues.column.label }}
-                        </option>
-                        <option
-                            v-for="value in columnWithValues.values"
-                            :key="value"
-                            value="{{value}}"
-                        >
-                            {{ value }}
-                        </option>
-                    </select>
-                </div>
-
-                <div class="submit-container">
-                    <button type="submit">Otsi</button>
-                </div>
-            </form>
-
-            <div class="result">
-                <div v-for="link in links" :key="link" class="link">
-                    <button
-                        @click.prevent="
-                            openUrl(
-                                link.dataCells[link.dataCells.length - 1].value
-                            )
-                        "
-                    >
-                        {{ link.dataCells[link.dataCells.length - 2].value }}
-                    </button>
-                </div>
-            </div>
-        </div>
-    </section>
+    <StepperComponent />
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
-import jsonLinks from "@/data/links.json";
+import StepperComponent from "@/components/home/StepperComponent.vue";
 
-export default class HomeView extends Vue {
-    hasNoData: boolean = true;
-    selectedLabels: string[] = [];
-    lastQuery: string[] = [];
-
-    created(): void {
-        console.log(jsonLinks);
-    }
-
-    getSelectedItems(event: any): void {
-        this.selectedLabels.push(
-            event.target.options[event.target.options.selectedIndex].text
-        );
-    }
-
-    openUrl(url: string): void {
-        window.open(url);
-    }
-}
+export default {
+    components: {
+        StepperComponent,
+    },
+};
 </script>
